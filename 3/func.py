520c0562e71db410cdf1fd0223b79938b6643a07
Kevin Hall <gtfour@gmail.com>
2023-10-04 22:39:25 -0700

    adding section 3

---
+++ 3/func.py
@@ -0,0 +1,14 @@
+def hello():
+    print('Howdy')
+    print('Howdy Howdy')
+    print('HELLO THERE!')
+
+hello()
+hello()
+hello()
+
+#notes
+#def command makes your own function 
+#printing a string makes it a "None" value similar to "True" and "False". Every function call has a return value.
+#function goal is to get rid of duplicate code
+#
---
+++ 3/sysexit.py
@@ -0,0 +1,8 @@
+import sys
+print('hello')
+sys.exit()
+print('see ya')
+
+#notes
+#line 4 never runs because of the exit 
+
