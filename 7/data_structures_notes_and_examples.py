42fa38212c68c2239c5db5c92674d300a5381388
Kevin Hall <gtfour@gmail.com>
2023-10-28 22:24:11 -0700

    adding data structure section with a few examples

---
+++ 7/data_structures_notes_and_examples.py
@@ -0,0 +1,10 @@
+#lesson 8 - data structures notes and examples
+
+cat = {'name': 'Tomato', 'age': 8, 'color': 'orange and black spots'}
+
+allcats = []
+allcats.append({'name': 'Tomato', 'age': 8, 'color': 'orange and black spots'})
+allcats.append({'name': 'Didi', 'age': 15, 'color': 'gray and black spots'})
+allcats.append({'name': 'MeanCat', 'age': 1, 'color': 'gray tabby'})
+
+#list of dictionaries is a data structure
